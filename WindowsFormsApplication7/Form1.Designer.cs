03e3b79a2110a7320f84cec9a876e3e07dde49c7
ironNPNcake <jakub.sadlo@outlook.com>
2016-12-15 01:41:17 +0100

    zaliczonko

---
+++ WindowsFormsApplication7/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormsApplication7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ustawToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(695, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ustawToolStripMenuItem,
+            this.startToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(782, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ustawToolStripMenuItem
+            // 
+            this.ustawToolStripMenuItem.Name = "ustawToolStripMenuItem";
+            this.ustawToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.ustawToolStripMenuItem.Text = "Ustaw";
+            this.ustawToolStripMenuItem.Click += new System.EventHandler(this.ustawToolStripMenuItem_Click);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 556);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ustawToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+
+        List<Button> przyciski = new List<Button>(4);
+        int nadKtorym = 1;
+        bool kontrola = false;
+        public Form1()
+        {
+            InitializeComponent();
+            przyciski.Add(new Button());
+            przyciski.Add(new Button());
+            przyciski.Add(new Button());
+            przyciski.Add(new Button());
+        }
+        string[] wejsciowyString = new string[4];
+        string[] pierwszyS, drugiS, trzeciS, czwartyS;
+
+        private void ustawToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+
+            for (int i = 0; i < 4; i++)
+            {
+                this.Controls.Add(przyciski[i]);
+
+            }
+            przyciski[0].Location = new Point(0, t1); przyciski[0].Width = w1;
+            przyciski[0].Width = w2; przyciski[1].Location = new Point(przyciski[0].Location.X + przyciski[0].Width, t2);
+            przyciski[1].Width = w3; przyciski[2].Location = new Point(przyciski[1].Location.X + przyciski[1].Width, t3);
+            przyciski[2].Width = w4; przyciski[3].Location = new Point(przyciski[2].Location.X + przyciski[2].Width, t4);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (przyciski[0].Location.Y + przyciski[0].Height < przyciski[3].Top)
+            {
+                if (przyciski[0].Location.Y + przyciski[0].Height < przyciski[nadKtorym].Location.Y)
+                    przyciski[0].Location = new Point(przyciski[0].Left, przyciski[0].Top + 1);
+                //if (przyciski[0].Location.Y+przyciski[0].Height > przyciski[nadKtorym].Location.Y) nadKtorym++;
+                if (przyciski[0].Location.Y + przyciski[0].Height == przyciski[nadKtorym].Location.Y)
+                    przyciski[0].Location = new Point(przyciski[0].Location.X + 1, przyciski[0].Location.Y);
+                if (przyciski[0].Location.X > przyciski[nadKtorym].Location.X + przyciski[nadKtorym].Width) nadKtorym++;
+            }
+            else { timer1.Stop(); kontrola = true; }
+
+
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            przyciski[0].Top += 2;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Right&&kontrola==true)
+                przyciski[0].Location = new Point(przyciski[0].Location.X + 10, przyciski[0].Location.Y);
+            if (przyciski[0].Location.X > przyciski[3].Location.X + przyciski[3].Width)
+            {
+                timer2.Start();
+            }
+
+        }
+
+        int w1, w2, w3, w4, t1, t2, t3, t4;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("abc.txt");
+            for(int i =0;i<4;i++)
+            {
+                wejsciowyString[i] = sr.ReadLine();
+            }
+            pierwszyS = wejsciowyString[0].Split(' ');
+            drugiS = wejsciowyString[1].Split(' ');
+            trzeciS = wejsciowyString[2].Split(' ');
+            czwartyS = wejsciowyString[3].Split(' ');
+            
+            t1 = int.Parse( pierwszyS[0]);
+            w1 = int.Parse( pierwszyS[1]);
+            t2 = int.Parse(drugiS[0]);
+            w2 = int.Parse(drugiS[1]);
+            t3 = int.Parse(trzeciS[0]);
+            w3 = int.Parse(trzeciS[1]);
+            t4 = int.Parse(czwartyS[0]);
+            w4 = int.Parse(czwartyS[1]);
+            //button1.Location = new Point(w1, t1);
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch(keyData)
+            {
+                case Keys.Down:
+                case Keys.Up:
+                case Keys.Left:
+                case Keys.Right:
+                    Form1_KeyDown(this, new System.Windows.Forms.KeyEventArgs(keyData));
+                    break;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
